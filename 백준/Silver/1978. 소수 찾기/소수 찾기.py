2072cc30c085bd11802d7a13470acaa3905c4105
eunewww <120937167+eunewww@users.noreply.github.com>
2023-03-03 22:43:05 +0900

    [Silver V] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,14 @@
+import sys
+
+def primecheck(a):
+    check= 1*(a>1)
+    for i in range(2,a):
+        if (a % i == 0):
+            check = 0
+            break
+    return check
+    
+n = int(sys.stdin.readline().strip())
+numbers = list(map(int, sys.stdin.readline().split()))
+answer = sum(primecheck(i) for i in numbers)
+print(answer)
