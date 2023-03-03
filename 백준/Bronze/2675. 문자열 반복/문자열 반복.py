79b038606008696f05ebc242927ca15045d98f24
eunewww <120937167+eunewww@users.noreply.github.com>
2023-03-03 19:23:18 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,5 +1,12 @@
-n = int(input())
-S = [input().split() for i in range(n)]
-for s in S:
-    answer = ''.join(list(int(s[0])*i for i in s[1]))
-    print(answer)
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+def SRP():
+    case = sys.stdin.readline().split()
+    word = ''
+    for i in case[1]:
+        word +=i*int(case[0])
+    print(word)
+
+for i in range(n): SRP()
