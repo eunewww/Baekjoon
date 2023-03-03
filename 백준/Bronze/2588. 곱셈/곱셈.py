4251fb70e75f163a9799decbc5e3494e34de604c
eunewww <120937167+eunewww@users.noreply.github.com>
2023-03-03 13:49:55 +0900

    Update 곱셈.py

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,3 +1,17 @@
+# 첫번째
 a = int(input())
 b = int(input())
 print(a*(b%10),a*((b//10)%10),a*((b//10)//10),a*b,sep='\n')
+
+# 두번째 
+# import sys
+# a= int(sys.stdin.readline().strip())
+# b= list(map(int, sys.stdin.readline().strip()))
+# result = 0
+# b.reverse()
+
+# for i, v in enumerate(b):
+#     print(a*v)
+#     result += a*v*(10**i)
+
+# print(result)
