17a445e14db9cfd5ae7daca3737309b760c8d4b0
eunewww <120937167+eunewww@users.noreply.github.com>
2023-03-04 13:25:36 +0900

    Update 소수 찾기.py

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -2,12 +2,13 @@ import sys
 
 def primecheck(a):
     check= 1*(a>1)
-    for i in range(2,a):
+    sr = int(a**0.5)
+    for i in range(2,sr+1):
         if (a % i == 0):
             check = 0
             break
     return check
-    
+     
 n = int(sys.stdin.readline().strip())
 numbers = list(map(int, sys.stdin.readline().split()))
 answer = sum(primecheck(i) for i in numbers)
